e04d2d1f77edd22f6b933998650fbfce286c618b
岡部歩美 <okabeayumi@okabeayuminoMacBook-Air.local>
2019-08-12 20:50:39 +0900

    S3_3

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock "~> 3.11.0"
 
 set :application, "chat-space"
 set :repo_url, "git@github.com:a-yun-ayuyu/chat-space.git"
+set :linked_files, %w{ config/secrets.yml }
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
